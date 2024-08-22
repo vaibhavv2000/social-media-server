2a314f9a80189b33b1e9f7539152e674a87b9bd1
vaibhav <vaibhavk1965@gmail.com>
2024-08-22 13:47:00 +0530

    Images...

--- app/config/sql.ts
+++ app/config/sql.ts
@@ -1,19 +1,28 @@
 import {createPool} from "mysql2/promise";
 import "dotenv/config";
 
-const sql = createPool({
- host: process.env.DB_HOST,
- user: process.env.DB_USER,
- password: process.env.DB_PWD,
- port: 10758,
- database: process.env.DB_NAME,
- ssl: {
-  rejectUnauthorized: false
- },
-});
+const config = {
+    host: "localhost",
+    user: "root",
+    database: "social",
+    password: "vaibhavk15"
+};
+
+const prod = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    port: 10758,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    },
+}
+
+const sql = createPool(config);
 
 sql.on("connection",() => {
- console.log("DB_CONNECTED");
+    console.log("DB_CONNECTED");
 });
 
 export default sql;
